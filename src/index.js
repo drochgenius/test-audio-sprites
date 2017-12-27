8495876e88bb76ab2fd99ed9f8cbb489c8d5d250
Dominique Rochefort <drochefort@tribalnova.com>
2017-12-27 12:45:10 -0500

    add some samples

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import { play } from './player';
 
-play('samples/rti/audio.mp3', 'samples/rti/manifest.json');
+/**
+ * @param {String} the path to a concatenated audio file
+ * @param {String} the path to the audio manifest file
+ */
+// play('samples/rti/audio.mp3', 'samples/rti/manifest.json');
+play('samples/sample_widget/c1e69e2b50744d7ba8080b6fc03b8ea2.mp3', 'samples/sample_widget/c1e69e2b50744d7ba8080b6fc03b8ea2.json');
