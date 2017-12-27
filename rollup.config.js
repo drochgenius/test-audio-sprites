48aa0057dacc320622c278d770656feab7b6e23d
Dominique Rochefort <drochefort@tribalnova.com>
2017-12-27 12:48:18 -0500

    format

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,16 @@
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 // rollup.config.js
-export default { // can be an array (for multiple inputs)
+export default {
+    // can be an array (for multiple inputs)
     // core input options
-    input: 'src/index.js',     // required
+    input: 'src/index.js', // required
 
-    output: {  // required (can be an array, for multiple outputs)
+    output: {
+        // required (can be an array, for multiple outputs)
         // core output options
-        file: 'build/index.js',    // required
-        format: 'iife',  // required
-
-
+        file: 'build/index.js', // required
+        format: 'iife' // required
     },
 
     plugins: [
--- src/index.js
+++ src/index.js
@@ -5,4 +5,7 @@ import { play } from './player';
  * @param {String} the path to the audio manifest file
  */
 // play('samples/rti/audio.mp3', 'samples/rti/manifest.json');
-play('samples/sample_widget/c1e69e2b50744d7ba8080b6fc03b8ea2.mp3', 'samples/sample_widget/c1e69e2b50744d7ba8080b6fc03b8ea2.json');
+play(
+    'samples/sample_widget/c1e69e2b50744d7ba8080b6fc03b8ea2.mp3',
+    'samples/sample_widget/c1e69e2b50744d7ba8080b6fc03b8ea2.json'
+);
