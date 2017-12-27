1350540b1871bb109bcf7bf0014c69db9457cf97
Dominique Rochefort <drochefort@tribalnova.com>
2017-12-27 12:54:15 -0500

    format

--- src/index.js
+++ src/index.js
@@ -4,8 +4,12 @@ import { play } from './player';
  * @param {String} the path to a concatenated audio file
  * @param {String} the path to the audio manifest file
  */
-// play('samples/rti/audio.mp3', 'samples/rti/manifest.json');
-play(
+
+// Working Sample
+play('samples/rti/audio.mp3', 'samples/rti/manifest.json');
+
+// Non-working sample
+/*play(
     'samples/sample_widget/c1e69e2b50744d7ba8080b6fc03b8ea2.mp3',
     'samples/sample_widget/c1e69e2b50744d7ba8080b6fc03b8ea2.json'
-);
+);*/
